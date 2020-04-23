152244e6f90993e5873f8f4f0fd6eab7f1e86e3b
ashar312 <ashar.ashfaq2@gmail.com>
2020-04-23 19:39:14 +0500

    connect disconnet done.

--- Server/server.js
+++ Server/server.js
@@ -1,10 +1,25 @@
 const path = require('path')
 const exrpress = require('express')
+const socketIO = require('socket.io')
+const http = require('http')
 const publicPath = path.join(__dirname, '/../Public')
 const app = exrpress();
 const port = process.env.PORT || 3001
+
+
+const server = http.createServer(app)
+const io = socketIO(server)
+
+io.on('connection', (socket) => {
+    console.log("New User Connected")
+
+    socket.on('disconnect',() => {
+        console.log("DisConnected To Server")
+      })
+})
 app.use(exrpress.static(publicPath));
-app.listen(port, ()=>{
+
+server.listen(port, ()=>{
     console.log("Port number is " + port)
 })
 
